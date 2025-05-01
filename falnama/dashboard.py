da6dd478ffd886b0859f2f57ac25d414c30bf9e1
Koen van Tilburg <koenvantilburg@gmail.com>
2025-05-01 13:52:38 +0200

    Initialising dashboards

--- falnama/dashboard.py
+++ falnama/dashboard.py
@@ -3,7 +3,7 @@
 # %% auto 0
 __all__ = ['Dashboard']
 
-# %% ../nbs/kitchen/20_exploring-our-maxrf-data.ipynb 12
+# %% ../nbs/kitchen/20_exploring-our-maxrf-data.ipynb 49
 import maxrf4u
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -14,7 +14,7 @@ import numpy as np
 import os
 import re
 
-# %% ../nbs/kitchen/20_exploring-our-maxrf-data.ipynb 13
+# %% ../nbs/kitchen/20_exploring-our-maxrf-data.ipynb 50
 class Dashboard:
 
     def __init__(self, datastack_files=None, roi_count=4, data=None):
