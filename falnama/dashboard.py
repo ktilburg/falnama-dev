a9937722c2c12586553bf4b6cd013a57263dbf58
Frank Ligterink <frank.ligterink@gmail.com>
2025-05-06 18:05:36 +0200

    switch python environment to avoid javascript error?

--- falnama/dashboard.py
+++ falnama/dashboard.py
@@ -3,7 +3,7 @@
 # %% auto 0
 __all__ = ['Dashboard']
 
-# %% ../nbs/kitchen/20_a-dashboard-for-exploring-our-maxrf-data.ipynb 46
+# %% ../nbs/kitchen/20_a-dashboard-for-exploring-our-maxrf-data.ipynb 48
 import maxrf4u
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
@@ -14,7 +14,7 @@ import numpy as np
 import os
 import re
 
-# %% ../nbs/kitchen/20_a-dashboard-for-exploring-our-maxrf-data.ipynb 47
+# %% ../nbs/kitchen/20_a-dashboard-for-exploring-our-maxrf-data.ipynb 49
 class Dashboard:
 
     def __init__(self, datastack_files=None, roi_count=4, data=None):
